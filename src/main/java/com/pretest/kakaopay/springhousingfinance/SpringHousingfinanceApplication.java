07a747641d7a0b378434cc664e7e396fab1753d5
sechun <sechun0215@gmail.com>
2019-04-04 15:16:14 +0900

    init project

---
+++ src/main/java/com/pretest/kakaopay/springhousingfinance/SpringHousingfinanceApplication.java
@@ -0,0 +1,13 @@
+package com.pretest.kakaopay.springhousingfinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHousingfinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHousingfinanceApplication.class, args);
+	}
+
+}
